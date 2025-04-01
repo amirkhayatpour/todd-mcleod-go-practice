218fd5a9f4f83a8eb5860c38e965eef8b70fb083
Amir <amirkhayatpour@gmail.com>
2025-04-01 15:47:49 +0330

    60

---
+++ my-practice/hands-on-exercises-16/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	fmt.Println("Hello Amir Hemati")
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
+}
