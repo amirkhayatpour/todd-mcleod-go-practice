f7faeb4e5f71f2f31b26b1b663ecd4524b575245
Amir <amirkhayatpour@gmail.com>
2025-04-26 19:18:05 +0330

    82

---
+++ my-practice/hands-on-exercises-24/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+import "math/rand"
+
+func main() {
+
+	for counter := 0; counter < 100; counter++ {
+		x := rand.Intn(10)
+		y := rand.Intn(10)
+
+		switch {
+		case x < 4 && y < 4:
+			fmt.Println("x and y are both less than 4")
+		case x > 6 && y > 6:
+			fmt.Println("x and y are both greater than 6")
+		case x >= 4 && x <= 6:
+			fmt.Println("x is greater than or equal to 4 and less than or equal to 6")
+		case y != 5:
+			fmt.Println("y is not 5")
+		default:
+			fmt.Println("none of the previous cases where met")
+		}
+	}
+
+}
