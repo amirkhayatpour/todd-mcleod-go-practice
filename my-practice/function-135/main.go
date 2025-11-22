764a73f4ce6f936b557e8857b762439175946db4
Amir <amirkhayatpour@gmail.com>
2025-11-22 20:19:15 +0330

    remember variadic parameter

--- my-practice/function-135/main.go
+++ my-practice/function-135/main.go
@@ -1,16 +1,18 @@
 package main
 
+//variadic parameter
+
 import "fmt"
 
 func main() {
+
 	sum(1, 2, 3, 4, 5, 6, 7, 8, 9)
 }
 
 func sum(numbers ...int) {
-	result := 0
-
-	for _, number := range numbers {
-		result += number
+	n := 0
+	for index, number := range numbers {
+		fmt.Printf("index: %v ---- number is : %v ---- and sum : %v\n", index, number, n)
+		n += number
 	}
-	fmt.Println(result)
 }
