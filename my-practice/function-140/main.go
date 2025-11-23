15fc4691524c4c2e1ae8fc7755385703d062d310
Amir <amirkhayatpour@gmail.com>
2025-11-23 11:42:26 +0330

    remember what is Stringer interface

--- my-practice/function-140/main.go
+++ my-practice/function-140/main.go
@@ -1,31 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
+
+// Stringer interface
 
 type book struct {
-	name string
+	readable
+	first string
+	pages int
 }
-
-type age int
 
 func (b book) String() string {
-	return fmt.Sprint("This is the book name: ", b.name)
+	return fmt.Sprint("hi")
 }
 
-func (a age) String() string {
-	return fmt.Sprint("And this is my Age: ", strconv.Itoa(int(a)))
+type readable struct {
+	alreadyReaded bool
 }
 
 func main() {
 	b := book{
-		name: "The Miserable Ones",
+		readable: readable{
+			true,
+		},
+		first: "binavayan",
+		pages: 200,
 	}
 
-	var n age = 30
-
 	fmt.Println(b)
-	fmt.Println(n)
 }
